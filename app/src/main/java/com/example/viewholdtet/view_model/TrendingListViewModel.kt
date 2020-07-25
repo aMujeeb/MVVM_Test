aa01ace037189ad03e4395c198efd101258c93b5
Ahamed Mujeeb <mujee2@gmail.com>
2020-07-25 20:57:30 +0530

    Implementing Retrofit with Kotlin Coroutines

--- app/src/main/java/com/example/viewholdtet/view_model/TrendingListViewModel.kt
+++ app/src/main/java/com/example/viewholdtet/view_model/TrendingListViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.viewholdtet.view_model
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.viewholdtet.model.AppUser
 import com.example.viewholdtet.model.Movie
 import com.example.viewholdtet.rest.repo.Repository
+import kotlinx.coroutines.launch
 
 class TrendingListViewModel : BaseViewModel() {
     val repoListLive = MutableLiveData<ArrayList<Movie>>()
@@ -11,10 +13,13 @@ class TrendingListViewModel : BaseViewModel() {
 
     fun requestWeeklyTrendingMovies() {
         mProgress.value = true
-        Repository.getInstance().getWeeklyTrendingList { isSuccess, results ->
-            mProgress.value = false
-            if (isSuccess) {
-                repoListLive.value = results
+
+        viewModelScope.launch {
+            Repository.getInstance().getWeeklyTrendingList { isSuccess, results ->
+                mProgress.value = false
+                if (isSuccess && !results.isNullOrEmpty()) {
+                    repoListLive.value = results
+                }
             }
         }
     }
